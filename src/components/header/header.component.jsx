be762531835a4d725c767d7b255acb448ddcfc94
kadokan81 <59529242+kadokan81@users.noreply.github.com>
2020-02-09 23:36:06 +0100

    Fix bad repo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import { selectCurentUser } from '../../redux/user/user.selector';
-import {  selectCartHidden } from '../../redux/cart/cart.selector';
-import { createStructuredSelector } from 'reselect';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -40,9 +40,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector ({
- currentUser: selectCurentUser ,
- hidden :selectCartHidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-   apiKey: "AIzaSyAeXTreFtLhPZFARp5k1kNyv1tUAvx5FpM",
-  authDomain: "react-shop-34a5a.firebaseapp.com",
-  databaseURL: "https://react-shop-34a5a.firebaseio.com",
-  projectId: "react-shop-34a5a",
-  storageBucket: "react-shop-34a5a.appspot.com",
-  messagingSenderId: "487284994598",
-  appId: "1:487284994598:web:a5cb501b53c6f0c861ace7"
+ apiKey: "AIzaSyAeXTreFtLhPZFARp5k1kNyv1tUAvx5FpM",
+    authDomain: "react-shop-34a5a.firebaseapp.com",
+    databaseURL: "https://react-shop-34a5a.firebaseio.com",
+    projectId: "react-shop-34a5a",
+    storageBucket: "react-shop-34a5a.appspot.com",
+    messagingSenderId: "487284994598",
+    appId: "1:487284994598:web:a5cb501b53c6f0c861ace7"
 };
 
 firebase.initializeApp(config);
